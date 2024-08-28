8ff6f3e4eaad9df404bd201088e62a6afbeca756
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2024-08-28 14:13:37 +0200

    feat: Add ASGI middleware (#15)

--- src/metrics_python/gunicorn/_metrics.py
+++ src/metrics_python/gunicorn/_metrics.py
@@ -5,7 +5,7 @@ from ..constants import NAMESPACE
 REQUEST_DURATION = Histogram(
     "request_duration",
     "Time spent on processing a request in Gunicorn",
-    ["status", "worker_pid"],
+    ["status", "method", "worker_pid"],
     unit="seconds",
     namespace=NAMESPACE,
     subsystem="gunicorn",
